76733b0e59484d0669a9e47fcb6d6cbeb733a5d2
Kevin Müller <kevin.mueller1@outlook.com>
2019-08-16 11:27:27 +0200

    Add project files.

---
+++ AdventOfCode/Days/Day7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public static class Day7
+    {
+        public static void Part1(string inputPath)
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day6.Solve(@"Input\day6.txt");
+            Day7.Part1(@"Input\day7.txt");
+            Console.Read();
+        }
+    }
+}
