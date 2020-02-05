0bc6ac0eb3e34a1cb32ef56d8211e66ccb695027
Kevin Müller <kevin.mueller1@outlook.com>
2020-02-05 15:16:55 +0100

    Working on day3

--- AdventOfCode/Days/Nineteen/Day3.cs
+++ AdventOfCode/Days/Nineteen/Day3.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,12 +10,27 @@ namespace AdventOfCode.Days.Nineteen
     {
         public static int Solve(string inputPath)
         {
-            string[] wiresRaw = File.ReadAllLines(inputPath);
-            var wire1 = new Wire(wiresRaw[0]);
-            var wire2 = new Wire(wiresRaw[1]);
 
-            //Find the same visitedPoints
-            //Calculate the closest distance to the start.
+            string[] wiresRaw = File.ReadAllLines(inputPath);
+
+            List<Wire> wires = new List<Wire>();
+            foreach (var item in wiresRaw)
+            {
+                wires.Add(new Wire(item));
+            }
+
+            var allPointsVisited1 = wires[0]._allPointsVisited;
+            var allPointsVisited2 = wires[1]._allPointsVisited;
+
+            var intersectingPoints = allPointsVisited1.Where(o => allPointsVisited2.Any(w => w.X == o.X && w.Y == o.Y));
+            List<int> distances = new List<int>();
+            foreach (var item in intersectingPoints)
+            {
+                distances.Add(item.CalculateDistanceToCentralPort());
+            }
+            distances.Sort();
+            //The first one is the central port
+            return distances[1];
         }
     }
 
@@ -22,13 +38,12 @@ namespace AdventOfCode.Days.Nineteen
     {
         private readonly List<string> _instructions = new List<string>();
 
-        private List<int> _allPointsVisited = new List<int>();
-
+        public List<Point> _allPointsVisited { get; set; }
 
         public Wire(string inputRaw)
         {
             _instructions = BuildInstructionTable(inputRaw);
-            ComputeAllPointsVisited(_instructions);
+            _allPointsVisited = ComputeAllPointsVisited(_instructions);
         }
 
         private List<string> BuildInstructionTable(string wireData)
@@ -40,33 +55,183 @@ namespace AdventOfCode.Days.Nineteen
             return _instructions;
         }
 
-        private List<int> ComputeAllPointsVisited(List<string> instructionTable)
+        private List<Point> ComputeAllPointsVisited(List<string> instructionTable)
         {
-            //TOOD Check for nullpointer exception.
+            //TOOD Check for index out of bounds exception.
             //TODO Idealy, scan the input an calculate the size of the matrix.
-            bool[,] matrix = new bool[9999, 9999];
-            
+            var matrix = new Matrix();
+
             //TODO loop through the matrix and set the points visited to true.
             foreach (var item in instructionTable)
             {
                 char direction = item[0];
+                int travelDistance = Convert.ToInt32(item.Remove(0, 1));
+                Console.WriteLine($"Traveling {direction} ({travelDistance})");
                 switch (direction)
                 {
                     case 'R':
-
+                        matrix.TravelRight(travelDistance);
                         break;
-                    case 'U':
 
+                    case 'U':
+                        matrix.TravelUp(travelDistance);
                         break;
 
                     case 'L':
-
+                        matrix.TravelLeft(travelDistance);
                         break;
-                    case 'D':
 
+                    case 'D':
+                        matrix.TravelDown(travelDistance);
                         break;
                 }
             }
+
+            //List<Point> pointsVisited = new List<Point>();
+            //for (int i = 0; i < matrix.SizeX; i++)
+            //{
+            //    for (int j = 0; j < matrix.SizeY; j++)
+            //    {
+            //        if (matrix.GetValueAt(i, j))
+            //        {
+            //            pointsVisited.Add(new Point(i, j));
+            //        }
+            //    }
+            //}
+            return matrix.GetMatrix();
+        }
+    }
+
+    internal class Point
+    {
+        public Point(int x, int y, bool visited = false)
+        {
+            X = x;
+            Y = y;
+            Visited = visited;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Visited { get; set; }
+        public bool IsCentralPort { get; set; }
+
+        public int CalculateDistanceToCentralPort()
+        {
+            return X + Y;
+        }
+    }
+
+    internal class Matrix
+    {
+        private const int MatrixSize = 9999999;
+
+        //Bigger Matrix => throws out of memory exception..
+        //use jagged array
+        private readonly List<Point> matrix = new List<Point>();
+
+        private readonly Point currentMatrixPosition = new Point(0, 0);
+
+        public readonly int SizeX;
+        public readonly int SizeY;
+
+        public Matrix()
+        {
+            //Central Port
+            matrix.Add(new Point(0, 0, true)
+            {
+                IsCentralPort = true
+            });
+        }
+
+        public List<Point> GetMatrix()
+        {
+            return matrix;
+        }
+
+        public bool GetValueAt(int x, int y)
+        {
+            return matrix.First(z => z.X == x && z.Y == y).Visited;
+        }
+
+        internal Point TravelRight(int travelDistance)
+        {
+            for (int i = 0; i < travelDistance; i++)
+            {
+                currentMatrixPosition.X += 1;
+                try
+                {
+                    //Found
+                    var element = matrix.First(z => z.X == currentMatrixPosition.X && z.Y == currentMatrixPosition.Y);
+                    element.Visited = true;
+                }
+                catch
+                {
+                    //Not found
+                    matrix.Add(new Point(currentMatrixPosition.X, currentMatrixPosition.Y, true));
+                }
+            }
+            return currentMatrixPosition;
+        }
+
+        internal Point TravelLeft(int travelDistance)
+        {
+            for (int i = 0; i < travelDistance; i++)
+            {
+                currentMatrixPosition.X -= 1;
+                try
+                {
+                    //Found
+                    var element = matrix.First(z => z.X == currentMatrixPosition.X && z.Y == currentMatrixPosition.Y);
+                    element.Visited = true;
+                }
+                catch
+                {
+                    //Not found
+                    matrix.Add(new Point(currentMatrixPosition.X, currentMatrixPosition.Y, true));
+                }
+            }
+            return currentMatrixPosition;
+        }
+
+        internal Point TravelUp(int travelDistance)
+        {
+            for (int i = 0; i < travelDistance; i++)
+            {
+                currentMatrixPosition.Y += 1;
+                try
+                {
+                    //Found
+                    var element = matrix.First(z => z.X == currentMatrixPosition.X && z.Y == currentMatrixPosition.Y);
+                    element.Visited = true;
+                }
+                catch
+                {
+                    //Not found
+                    matrix.Add(new Point(currentMatrixPosition.X, currentMatrixPosition.Y, true));
+                }
+            }
+            return currentMatrixPosition;
+        }
+
+        internal Point TravelDown(int travelDistance)
+        {
+            for (int i = 0; i < travelDistance; i++)
+            {
+                currentMatrixPosition.Y -= 1;
+                try
+                {
+                    //Found
+                    var element = matrix.First(z => z.X == currentMatrixPosition.X && z.Y == currentMatrixPosition.Y);
+                    element.Visited = true;
+                }
+                catch
+                {
+                    //Not found
+                    matrix.Add(new Point(currentMatrixPosition.X, currentMatrixPosition.Y, true));
+                }
+            }
+            return currentMatrixPosition;
         }
     }
 }
