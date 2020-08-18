2481feff102253b19ce4913ca781196e3a57ada6
Kevin Müller <kevin.mueller1@outlook.com>
2020-08-18 15:59:14 +0200

    finished day4 part2

--- AdventOfCodeCore/Days/Nineteen/Day4.cs
+++ AdventOfCodeCore/Days/Nineteen/Day4.cs
@@ -35,18 +35,48 @@ namespace AdventOfCode.Days.Nineteen
         {
             string startString = number.ToString();
 
+            //check for declining values
             for (int i = 1; i < startString.Length; i++)
             {
                 if (Convert.ToInt16(startString[i].ToString()) < Convert.ToInt16(startString[i - 1].ToString()))
                     return false;
             }
 
+            //get all adjacent values
+            List<string> adjacentValues = new List<string>();
+            for (int i = 0; i < startString.Length; i++)
+            {
+                if (adjacentValues.Any(x => x.Contains(startString[i])))
+                    continue;
+
+                string matchingChars = startString[i].ToString();
+                for (int j = i; j < startString.Length; j++)
+                {
+                    if (j + 1 == startString.Length)
+                        break;
+
+                    if (startString[i].Equals(startString[j + 1]))
+                    {
+                        matchingChars += startString[i].ToString();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (matchingChars.Length > 1)
+                    adjacentValues.Add(matchingChars);
+            }
+            adjacentValues = adjacentValues.Distinct().ToList();
+            if (!adjacentValues.Any(x => x.Count() == 2))
+                return false;
+
 
             for (int i = 0; i < startString.Length; i++)
             {
                 if (i + 1 == startString.Length)
                     break;
-                
+
                 if (startString[i].Equals(startString[i + 1]))
                 {
                     return true;
